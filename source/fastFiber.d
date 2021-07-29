4d47dab7dccba2f5fa2e87b1439d10f29ff16be3
Stefan Koch <uplink.coder@googlemail.com>
2021-07-29 12:26:11 +0200

    First source commit

---
+++ source/fastFiber.d
@@ -0,0 +1,21 @@
+
+struct Context
+{
+  size_t ax;
+  size_t bx;
+  size_t cx;
+  size_t dx;
+  size_t sp;
+  size_t bp;
+  size_t ip;
+  size_t r08;
+  size_t r09;
+  size_t r10;
+  size_t r11;
+  size_t r12;
+  size_t r13;
+  size_t r14;
+  size_t r15;
+
+  size_t reg[16];
+}
