5562a873358fceffd878c171b9cc77eda6763b16
Stefan Koch <uplink.coder@googlemail.com>
2021-10-28 03:25:20 +0200

    Add beginning for new taskgroup

---
+++ source/build_ldc.sh
@@ -0,0 +1,1 @@
+ldc2 myFiber_ldc.d myFiber.d test_task.d ticket.d intel_inspector.d app.d taskfiber.d -g -O3 #-d-version=pmutex
---
+++ source/tg.d
@@ -0,0 +1,7 @@
+TaskGroup tg;
+
+tg.addTask(Task((Task* task)
+{
+    auto s = *cast (string*) task.arg;
+    
+}), alloc.String("Hello World"), cast(void*)&hash));
