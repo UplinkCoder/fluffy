b7ed2a572b51fa830f083dedab36713dfdd3c8a9
Stefan Koch <uplink.coder@googlemail.com>
2021-10-30 00:55:47 +0200

    add taskgroup to build-script

--- source/build_ldc.sh
+++ source/build_ldc.sh
@@ -1,1 +1,1 @@
-ldc2 myFiber_ldc.d myFiber.d test_task.d ticket.d intel_inspector.d app.d taskfiber.d taskqueue.d -g -O0 
+ldc2 myFiber_ldc.d myFiber.d test_task.d ticket.d intel_inspector.d app.d taskfiber.d taskqueue.d taskgroup.d -g -O0 
